d6ab0789055a751ec348f851665722f732b46a91
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-08-07 02:16:34 +0900

    feat: storybookのchakura-ui用設定を追加

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,44 @@
+const path = require('path')
+
+const toPath = (_path) => path.join(process.cwd(), _path)
+
 module.exports = {
   stories: [
     '../src/stories/**/*.stories.mdx',
-    '../src/stories/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/stories/**/*.stories.@(js|jsx|ts|tsx)'
   ],
+  refs: {
+    '@chakra-ui/react': { disable: true }
+  },
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@chakra-ui/storybook-addon'
   ],
   framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-webpack5',
+  features: {
+    emotionAlias: false
   },
+  core: {
+    builder: '@storybook/builder-webpack5'
+  },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto'
+    })
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          '@emotion/core': toPath('node_modules/@emotion/react'),
+          'emotion-theming': toPath('node_modules/@emotion/react')
+        }
+      }
+    }
+  }
 }
