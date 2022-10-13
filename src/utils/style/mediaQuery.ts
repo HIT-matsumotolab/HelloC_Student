c3a81b1d37a45759bd7910a209d1110234dbd2a6
marucircle <2000ryo0113@ezweb.ne.jp>
2022-10-13 21:51:37 +0900

    feat: 定数追加

--- src/utils/style/mediaQuery.ts
+++ src/utils/style/mediaQuery.ts
@@ -1,11 +1,12 @@
 import { css, CSSObject } from 'styled-components'
+import { mediaQuerySize } from '../../constants/mediaQuerySize'
 
 export const mediaQuery = {
   sp: (
     sp: CSSObject | TemplateStringsArray,
     ...args: TemplateStringsArray[]
   ) => css`
-    @media (max-width: 700px) {
+    @media (max-width: ${mediaQuerySize.sp}) {
       ${css(sp, ...args)};
     }
   `
