2e132a6456b607b94a6cc6321d2b207e09413d08
marucircle <2000ryo0113@ezweb.ne.jp>
2022-10-30 21:27:27 +0900

    feat: Navigationをコンポーネントとして分離

--- src/components/layouts/MainPageLayout.tsx
+++ src/components/layouts/MainPageLayout.tsx
@@ -11,6 +11,7 @@ import { fontSize } from '../../constants/fontSize'
 import Link from 'next/link'
 import { mediaQuery } from '../../utils/style/mediaQuery'
 import Image from 'next/image'
+import { Navigation } from '../app/Navigation'
 
 const StyledMainPageLayout = styled.div`
   display: grid;
@@ -34,67 +35,6 @@ const MainPageHeader = styled.header`
   `}
 `
 
-//TODO Navigationのコード量が多いのでコンポーネントとして分離した方が良さそうだ
-
-const Navigation = styled.nav<{ isOpen: boolean }>`
-  position: fixed;
-  top: 0;
-  left: ${({ isOpen }) => (isOpen ? '0' : '-300px')};
-  height: 100vh;
-  overflow-y: scroll;
-  background-color: ${backgroundColor.white};
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  row-gap: 40px;
-  padding: ${padding.lg} 0;
-  transition: left 0.3s ease-out;
-  box-shadow: 0 2px 4px -1px rgba(0, 0, 0, 0.25);
-  z-index: ${zIndex.navigation};
-`
-
-const NavigationHeader = styled.div`
-  padding: 0 ${padding.md};
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  align-items: center;
-`
-
-const NavigationLinkTabList = styled.ul`
-  list-style: none;
-`
-
-const NavigationLinkTab = styled.li`
-  text-align: center;
-  transition: background-color 0.3s;
-
-  &:hover {
-    background-color: #66666650;
-  }
-`
-
-const NavigationFooter = styled.ul`
-  display: flex;
-  flex-direction: column;
-`
-
-const NavigationFooterTab = styled.li`
-  text-align: center;
-  transition: background-color 0.3s;
-  cursor: pointer;
-
-  &:hover {
-    background-color: #66666650;
-  }
-`
-
-const BlockLink = styled.a`
-  display: block;
-  padding: ${padding.sm} 0;
-  cursor: pointer;
-`
-
 const MainPageLayout = ({ children }: { children: JSX.Element }) => {
   const [isOpenNavigation, setIsOpenNavigation] = useState(false)
   const router = useRouter()
@@ -103,6 +43,10 @@ const MainPageLayout = ({ children }: { children: JSX.Element }) => {
   const logoutHandler = () => {
     removeCookie('HelloC')
     router.push('/login')
+  }
+
+  const onCloseNavigationHandler = () => {
+    setIsOpenNavigation(false)
   }
 
   useEffect(() => {
@@ -123,52 +67,11 @@ const MainPageLayout = ({ children }: { children: JSX.Element }) => {
         <div>HelloC for Student</div>
       </MainPageHeader>
       <main>{children}</main>
-      <Navigation isOpen={isOpenNavigation}>
-        <NavigationHeader>
-          <Image alt="Logo" src="/logo.svg" width="50" height="50" />
-          <div>ようこそ、〇〇さん</div>
-        </NavigationHeader>
-        <NavigationLinkTabList>
-          <NavigationLinkTab>
-            <Link href="/">
-              <BlockLink>メインページ</BlockLink>
-            </Link>
-          </NavigationLinkTab>
-          <NavigationLinkTab>
-            <Link href="#">
-              <BlockLink>クラスの管理</BlockLink>
-            </Link>
-          </NavigationLinkTab>
-          <NavigationLinkTab>
-            <Link href="#">
-              <BlockLink>問題集の管理</BlockLink>
-            </Link>
-          </NavigationLinkTab>
-          <NavigationLinkTab>
-            <Link href="#">
-              <BlockLink>進捗の管理</BlockLink>
-            </Link>
-          </NavigationLinkTab>
-          <NavigationLinkTab>
-            <Link href="#">
-              <BlockLink>学習ログの管理</BlockLink>
-            </Link>
-          </NavigationLinkTab>
-        </NavigationLinkTabList>
-        <NavigationFooter>
-          <NavigationFooterTab>
-            <Link href="#">
-              <BlockLink>ユーザー情報確認</BlockLink>
-            </Link>
-          </NavigationFooterTab>
-          <NavigationFooterTab onClick={logoutHandler}>
-            <BlockLink>ログアウト</BlockLink>
-          </NavigationFooterTab>
-        </NavigationFooter>
+      <Navigation isOpen={isOpenNavigation} onClose={onCloseNavigationHandler}>
+        <Navigation.Header />
+        <Navigation.LinkContent />
+        <Navigation.Footer logoutHandler={logoutHandler} />
       </Navigation>
-      {isOpenNavigation && (
-        <Overlay onClick={() => setIsOpenNavigation(false)} />
-      )}
     </StyledMainPageLayout>
   )
 }
